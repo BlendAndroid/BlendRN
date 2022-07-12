5284c9304c27115a308452baf72c12f91b1334a5
BlendAndroid <1059152860@qq.com>
2022-07-12 17:18:29 +0800

    feat:核心基础-Style

---
+++ basics/03_Style/StyleApp.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Images from "./aspectRatio";
+
+export default function StyleApp() {
+    return (
+        <Images/>
+    );
+}
--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-import ComponentApp from "./basics/02_Component/ComponentApp";
+import StyleApp from "./basics/03_Style/StyleApp";
 
 
-AppRegistry.registerComponent(appName, () => ComponentApp);
+
+AppRegistry.registerComponent(appName, () => StyleApp);
