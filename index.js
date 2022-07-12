8cb4758e4ae32365d2d36bf9ed8f0c781cf7861c
BlendAndroid <1059152860@qq.com>
2022-07-12 15:40:49 +0800

    feat:核心基础-Component

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-import ComponentApp from "./basics/component/ComponentApp";
+import ComponentApp from "./basics/02_Component/ComponentApp";
 
 
 AppRegistry.registerComponent(appName, () => ComponentApp);
